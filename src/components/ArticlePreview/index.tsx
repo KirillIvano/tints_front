1d99b8602bf7d98b8625206d523d0bc17d1ba149
KirillIvano <ivannadez@yandex.ru>
2021-06-07 00:42:44 +0300

    article preview component

---
+++ src/components/ArticlePreview/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import {Typo, Reference} from '@/uikit';
+
+import styles from './styles.module.scss';
+
+
+export type ArticlePreviewProps = {
+    caption: string;
+    image: string;
+    href: string;
+}
+
+const ArticlePreview = ({
+    caption,
+    image,
+    href,
+}: ArticlePreviewProps) => (
+    <Reference
+        href={href}
+        className={styles.preview__link}
+    >
+        <div
+            className={styles.preview}
+            style={{backgroundImage: `url(${image})`}}
+        >
+            <div className={styles.preview__overlay}>
+                <Typo
+                    as="span"
+                    color="white"
+                    size="xl"
+                    weight="bold"
+                >
+                    {caption}
+                </Typo>
+            </div>
+
+            <div aria-hidden="true" className={styles.preview__button} />
+        </div>
+    </Reference>
+);
+
+export default ArticlePreview;
