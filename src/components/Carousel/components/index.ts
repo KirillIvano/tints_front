8fefd450c98f564ab90084d7a109f8614b3f6e26
KirillIvano <ivannadez@yandex.ru>
2021-06-20 14:14:15 +0300

    removed old carousel

--- src/components/Carousel/components/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as CarouselContent} from './CarouselContent';
-export {default as CarouselItems} from './CarouselItems';
-export {default as CarouselProgress} from './CarouselProgress';
-
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
 export {default as ArticlePreview} from './ArticlePreview';
-export {default as Carousel} from './Carousel';
 export {default as Testimonial} from './Testimonial';
