a20954c82d8300dd6aa7710042b6468699008d25
KirillIvano <ivannadez@yandex.ru>
2021-06-27 21:57:33 +0300

    build fixes

--- src/uikit/Input/index.tsx
+++ src/uikit/Input/index.tsx
@@ -4,71 +4,9 @@ import classnames from 'classnames';
 import styles from './styles.module.scss';
 
 
-type InputBaseProps = {
-    className: string;
-    id: string;
-    name: string;
-
-    placeholder?: string;
-    type?: string;
-    value?: string;
-    tabIndex?: number;
-    sizing?: 'sm' | 'lg';
-    disabled?: boolean;
-}
-
-type InputHandlers<TInputType extends HTMLElement> = {
-    onChange?: (e: React.ChangeEvent<TInputType>) => void;
-    onClick?: (e: React.MouseEvent<TInputType>) => void;
-    onKeyUp?: (e: React.KeyboardEvent<TInputType>) => void;
-    onKeyDown?: (e: React.KeyboardEvent<TInputType>) => void;
-    onFocus?: (e: React.FocusEvent<TInputType>) => void;
-    onBlur?: (e: React.FocusEvent<TInputType>) => void;
-}
-
-type TextAreaProps =  {
-    multiline: true;
-} & InputBaseProps & InputHandlers<HTMLTextAreaElement>;
-
-type InputProps = {
-    multiline?: false;
-}  & InputBaseProps & InputHandlers<HTMLInputElement>;
-
-
-export const Input = React.forwardRef<unknown, TextAreaProps | InputProps>(({
-    className,
-    sizing='sm',
-    multiline,
-
-    ...props
-}: InputProps | TextAreaProps, ref) => {
-    const commonProps = {
-        className: classnames(
-            className,
-            styles.input,
-            {[styles.large]: sizing === 'lg'},
-        ),
-    };
-
-    if (multiline) {
-        return (
-            <textarea
-                {...props as TextAreaProps}
-                {...commonProps}
-                ref={ref}
-            />
-        );
-    }
-
-    return <input {...props as InputProps} {...commonProps} type="text" />;
-});
-Input.displayName = 'Input';
-
 interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
     sizing: 'sm' | 'lg';
 }
-
-
 
 export const Select = ({
     className,
--- src/uikit/index.ts
+++ src/uikit/index.ts
@@ -1,5 +1,5 @@
 export {default as Typo} from './Typography';
-export {Input, Select} from './Input';
+export {Select} from './Input';
 export {default as Box} from './Box';
 export {default as Reference} from './Reference';
 export {default as Button} from './Button';
