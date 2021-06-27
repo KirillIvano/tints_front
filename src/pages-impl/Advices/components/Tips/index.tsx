ef20f7d8077e09d2097d8fb006a170df4227bd06
KirillIvano <ivannadez@yandex.ru>
2021-06-27 17:08:11 +0300

    moved advices

--- src/pages-impl/Advices/components/Tips/index.tsx
+++ src/pages-impl/Advices/components/Tips/index.tsx
@@ -12,42 +12,42 @@ const Tips = ({className}: StyledProps) => (
         <div className={cn('col-xs-12 col-md-4', styles.tip)}>
             <ArticlePreview
                 caption="Советы по перманентным краскам"
-                href="/TODO"
-                image="/faqs/permanentPaints.png"
+                href="/articles/permament-tips"
+                image="/faqs/permamentPaints.png"
             />
         </div>
         <div className={cn('col-xs-12 col-md-4', styles.tip)}>
             <ArticlePreview
                 caption="Как использовать Henna Cream"
-                href="/TODO"
+                href="/articles/henna-cream"
                 image="/faqs/hennaCream.png"
             />
         </div>
         <div className={cn('col-xs-12 col-md-4', styles.tip)}>
             <ArticlePreview
                 caption="Окрашивание светлых волос"
-                href="/TODO"
+                href="/articles/lightener-tips"
                 image="/faqs/lightHair.png"
             />
         </div>
         <div className={cn('col-xs-12 col-md-4', styles.tip)}>
             <ArticlePreview
                 caption={<span>Тестируем кожу&nbsp;и&nbsp;волосы</span>}
-                href="/TODO"
+                href="/articles/hair-skin-tests"
                 image="/faqs/hairTest.png"
             />
         </div>
         <div className={cn('col-xs-12 col-md-4', styles.tip)}>
             <ArticlePreview
                 caption="Уход за волосами"
-                href="/TODO"
+                href="/articles/hair-care-tips"
                 image="/faqs/hairCare.png"
             />
         </div>
         <div className={cn('col-xs-12 col-md-4', styles.tip)}>
             <ArticlePreview
                 caption="Правильное высветление"
-                href="/TODO"
+                href="/articles/lightener-tips"
                 image="/faqs/highlighting.png"
             />
         </div>
